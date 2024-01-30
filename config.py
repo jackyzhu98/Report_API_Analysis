de7f3612ad5be130697f5f2b5b43f834e3def7b9
jackzhu98 <boltzhu98@gmail.com>
2024-01-30 15:47:03 +0800

    111

---
+++ config.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Jan 30 2024
+@author: Jiaqi Zhu
+
+"""
+import os
+import datetime
+import numpy as np
+import pandas as pd
+
+
+###############
+#Company Configs
+###############
+company_name = "QiuDe"
+# 1 - 松华 2 - 球德 3 - 沃鸿翔
+company_ID = 2
+
+##########
+#输出路径##
+##########
+path = "D:\合作店铺"
+output_path = os.path.join(path,company_name)
+
+#################
+#报告所需其他参数：
+#################
+
+report_year = 2023 # 年份
+
+###########
+#数据库参数#
+###########
+## Connection
+
+db_host = '192.168.2.108'
+db_user= 'jacky'
+password= 'jacky_1017'
+
+
+## Database
+api_db = 'amazon_swagger'
+background_db = 'zonglink_amazon_v9'
+currency_db = 'analyze_support_data'
+
+## Tables
+
+### Tables in API_DB
+order_table = 'tb_orders_v0_orders_orders'
+order_events = 'tb_finances_v0_financialevents_shipmenteventlist'
+order_details = 'tb_finances_v0_financialevents_shipmentitemlist'
+finance_table =  'tb_finances_v0_financialeventgroups_financialeventgrouplist'
+refund_table  = 'tb_finances_v0_financialevents_chargebackeventlist'
+refund_details = 'tb_finances_v0_financialevents_shipmentitemadjustmentlist'
+inv_table = 'tb_fba_inventory_v1_inventorysummaries'
+
+### Tables in BG_DB
+seller_table = 'vw_company_email_seller'
+seller_product = 'tb_product_info_us'
+
+### Tables in Currency_DB
+currency_table = 'exchange_rate_daily'
---
+++ queries.py
@@ -0,0 +1,106 @@
+from config import *
+import mysql.connector
+
+### Seller 筛选条件
+seller_filter = f""" _seller_id collate utf8mb4_unicode_520_ci in 
+                (SELECT seller_code FROM zonglink_amazon_v9.vw_company_email_seller
+                where company_id = {company_ID})"""
+order_filter = f""" amount <> '' and order_status = 'Shipped' """ #数额大于0且成功送达
+
+###############
+#####货币汇率###
+###############
+
+cur_query =f""" 
+            SELECT currency_code,exchange_rate,year,month,day 
+            FROM analyze_support_data.exchange_rate_daily 
+            WHERE year > 2021
+            """ # 大概率只需要2022年后的汇率
+
+###############
+###店铺-订单记录#
+###############
+seller_order_qurcols =  ['_seller_id', 'currency_code', 'amount','number_of_items_shipped','sales_channel','amazon_order_id','Year(purchase_date)','Month(purchase_date)']
+seller_order_query = f"""
+            SELECT {', '.join(seller_order_qurcols)}
+            FROM {'.'.join([api_db,order_table])}
+            WHERE {seller_filter} and {order_filter}
+                        """
+seller_order_cols =  ['seller_id', 'currency_code', 'amount','quantity_shipped','marketplace','amazon_order_id','year','month']
+
+###############
+###店铺-产品记录##
+###############
+seller_inventory_query = f"""
+                    SELECT _seller_id,seller_sku,asin,total_quantity
+                    FROM tb_fba_inventory_v1_inventorysummaries
+                    WHERE {seller_filter}"""
+seller_inventory_cols = ['seller_id','seller_sku','asin','total_quantity']
+###############
+###订单-产品信息#
+###############
+
+order_product_cols =  ['amazon_order_id','marketplace','year','month','seller_sku','quantity_shipped','charge_list','fee_list','tax_list']
+order_product_query = f"""
+            SELECT a.amazon_order_id,a.sales_channel, a.year,a.month, c.seller_s_k_u,c.quantity_shipped,c.item_charge_list,c.item_fee_list,c.item_tax_withheld_list
+            FROM
+
+            (SELECT _seller_id,amazon_order_id,sales_channel,Year(purchase_date) year,Month(purchase_date) month
+            FROM {'.'.join([api_db,order_table])}
+            WHERE {seller_filter} and {order_filter}) a 
+            
+            INNER JOIN
+            
+            (SELECT id, amazon_order_id
+            FROM {'.'.join([api_db,order_events])}) b
+            on a.amazon_order_id = b.amazon_order_id
+            
+            INNER JOIN
+
+            (SELECT _root_id,seller_s_k_u,quantity_shipped, item_charge_list, item_fee_list,item_tax_withheld_list
+            FROM {'.'.join([api_db,order_details])}) c
+            ON b.id = c._root_id
+            """
+
+###############
+###退单记录#####
+###############
+
+charge_back_cols = ['seller_id','amazon_order_id','marketplace','seller_sku','quantity_shipped','charge_adj_list','fee_adj_list','tax_adj_list','year','month']
+charge_back_query = f"""
+            SELECT a._seller_id,b.amazon_order_id, b.marketplace_name,a.seller_s_k_u,a.quantity_shipped,a.item_charge_adjustment_list,a.item_fee_adjustment_list,a.item_tax_withheld_list,c.year,c.month
+            FROM
+                (SELECT _root_id,_seller_id,seller_s_k_u,order_adjustment_item_id,quantity_shipped,item_charge_adjustment_list,item_fee_adjustment_list,item_tax_withheld_list 
+                        FROM {'.'.join([api_db,refund_details])} 
+                        WHERE 
+                        _parent_table = '{refund_table}' 
+                        and {seller_filter}) a
+                INNER JOIN
+                (SELECT id, amazon_order_id, marketplace_name
+                    FROM  {'.'.join([api_db,refund_table])}
+                    WHERE {seller_filter}) b
+                ON 
+                    a. _root_id = b.id
+                LEFT JOIN
+                (SELECT distinct amazon_order_id, year(purchase_date) year, month(purchase_date) month, day(purchase_date) day
+                    FROM tb_orders_v0_orders_orders
+                    WHERE {seller_filter}) c
+                ON b.amazon_order_id = c.amazon_order_id
+"""
+
+raw_query_list = ['seller_order','seller_inventory','order_product','charge_back']
+raw_query_dict = {'seller_order':seller_order_query,'seller_inventory':seller_inventory_query,'order_product':order_product_query,'charge_back':charge_back_query}
+raw_col_dict = {'seller_order':seller_order_cols,'seller_inventory':seller_inventory_cols,'order_product':order_product_cols,'charge_back':charge_back_cols}
+
+##############
+def query_func(connection,query,col_name):
+    cursor = connection.cursor()
+    cursor.execute(query)
+    query_result = cursor.fetchall()
+    query_result = pd.DataFrame(query_result)
+    query_result.columns = col_name
+    print(query_result.shape)
+    query_result = query_result.drop_duplicates()
+    print(query_result.shape)
+    cursor.close()
+    return query_result
